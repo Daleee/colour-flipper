4ce7b2e4d63e04ffdcff1efb2c796c58d9a06142
Dale Bobermien <dalebobermien@outlook.com>
2023-03-03 19:47:44 +1000

    final changes

--- hex.js
+++ hex.js
@@ -8,6 +8,8 @@ btn.addEventListener("click", function () {
   colour.textContent = randomHex;
 });
 
+//The below function generates a random integer between 0 and 16777215, which represents all possible combinations of 6 hexadecimal digits. It then converts this integer to a hex string and pads it with leading zeros if necessary to ensure that it is exactly 6 digits long. Finally, it returns the hex string with a '#' prefix to make it a valid hex code.
+
 function getRandomHexCode() {
   // Generate a random integer between 0 and 16777215 (0xffffff in decimal)
   const randomInt = Math.floor(Math.random() * 16777215);
